bb10d2d9e35c1506db2503df768fa10ee6c8b7b4
buzz-code <hadasa.schechter@gmail.com>
2023-01-31 19:46:05 +0000

    remove redundant endpoints

--- base-entity/base-entity.controller.ts
+++ base-entity/base-entity.controller.ts
@@ -1,5 +1,4 @@
-import { Get, StreamableFile, UseInterceptors } from "@nestjs/common";
-import { CrudController, CrudRequest, CrudRequestInterceptor, ParsedRequest } from "@dataui/crud";
+import { CrudController, CrudRequest } from "@dataui/crud";
 import { ExportFormats, ExportedFileResponse } from "@shared/exporter/types";
 import { getExportedFile } from "@shared/exporter/exporterUtil";
 import { BaseEntityService } from "./base-entity.service";
@@ -8,27 +7,12 @@ import { Entity } from "./interface";
 export class BaseEntityController<T extends Entity> implements CrudController<T> {
     constructor(public service: BaseEntityService<T>) { }
 
-    protected async exportFile(req: CrudRequest, format: ExportFormats): Promise<ExportedFileResponse> {
-        const data = await this.service.getDataForExport(req);
-        return getExportedFile(format, this.service.getName(), data, this.service.getExportHeaders());
-    }
-
-
-    @Get('/get-count')
-    @UseInterceptors(CrudRequestInterceptor)
-    getCount(@ParsedRequest() req: CrudRequest) {
+    getCount(req: CrudRequest) {
         return this.service.getCount(req);
     }
 
-    @Get('/export/excel')
-    @UseInterceptors(CrudRequestInterceptor)
-    exportExcel(@ParsedRequest() req: CrudRequest) {
-        return this.exportFile(req, ExportFormats.Excel);
-    }
-
-    @Get('/export/pdf')
-    @UseInterceptors(CrudRequestInterceptor)
-    exportPdf(@ParsedRequest() req: CrudRequest) {
-        return this.exportFile(req, ExportFormats.Pdf);
+    protected async exportFile(req: CrudRequest, format: ExportFormats): Promise<ExportedFileResponse> {
+        const data = await this.service.getDataForExport(req);
+        return getExportedFile(format, this.service.getName(), data, this.service.getExportHeaders());
     }
 }
