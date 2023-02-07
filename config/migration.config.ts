5f3d35266806591c6d115977194be50ca4fab400
buzz-code <hadasa.schechter@gmail.com>
2023-02-07 10:43:52 +0000

    add entities file path to migration config

--- config/migration.config.ts
+++ config/migration.config.ts
@@ -1,5 +1,8 @@
 import { DataSource } from "typeorm";
 import { databaseConfig } from "./database.config";
 
-const dataSource = new DataSource(databaseConfig);
+const dataSource = new DataSource({
+    ...databaseConfig,
+    entities: [__dirname + '/../../src/db/*/*.js'],
+});
 export default dataSource;
