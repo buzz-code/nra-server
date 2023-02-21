a4f5d9c68ec7cca541a5ec7cbfc91430ffd7da65
buzz-code <hadasa.schechter@gmail.com>
2023-02-21 18:21:57 +0000

    update yemot interface

--- utils/yemot/yemot.interface.ts
+++ utils/yemot/yemot.interface.ts
@@ -1,9 +1,14 @@
 import { YemotCall, YemotParams } from "@shared/entities/YemotCall.entity";
+import { Text } from "src/db/entities/Text.entity";
 import { DataSource } from "typeorm";
 
 export const YEMOT_PROCCESSOR_PROVIDER = 'yemot_processor_provider';
 export const YEMOT_HANGUP_STEP = 'hangup';
 export const YEMOT_NOT_IMPL_STEP = 'error-not-impl-step';
+
+export function FormatString(str: string, val: string[]) {
+  return str.replace(/{([\d]*)}/g, (_, index) => val[index]);
+}
 
 export abstract class YemotProcessor {
   constructor(protected dataSource: DataSource) { }
@@ -11,9 +16,12 @@ export abstract class YemotProcessor {
   steps: { [key: string]: string };
   params: { [key: string]: string };
 
-  protected async getText(textKey: string, ...args): Promise<string> {
-    // todo: use cache
-    return 'text: ' + textKey + args.join(', ');
+  protected async getText(userId: number, textKey: string, ...args): Promise<string> {
+    const text = await this.dataSource.getRepository(Text).findOne({
+      where: { userId, name: textKey },
+      cache: true,
+    })
+    return FormatString(text.value, args);
   }
 
   abstract processCall(activeCall: YemotCall, body: YemotParams): Promise<{ response: string; nextStep: string; }>;
