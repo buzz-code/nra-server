daac0d37109c6e06a69821fe02738b6af0322363
buzz-code <hadasa.schechter@gmail.com>
2023-04-15 20:42:00 +0000

    add logic to validate mail uniquness

---
+++ config/mail-workflows.ts
@@ -0,0 +1,4 @@
+export const mailWorkflowUrls = {
+    saveMailAddress: 'https://n8n.yoman.online/webhook/save-mail-address',
+    validateMailQnique: 'https://n8n.yoman.online/webhook/validate-mail-uniqueness',
+};
--- utils/mail/mail-address-update.interceptor.ts
+++ utils/mail/mail-address-update.interceptor.ts
@@ -1,19 +1,31 @@
 import { HttpService } from '@nestjs/axios';
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { BadRequestException, CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { mailWorkflowUrls } from '@shared/config/mail-workflows';
 import { MailAddress } from '@shared/entities/MailAddress.entity';
-import { firstValueFrom, Observable, tap } from 'rxjs';
+import { Request } from 'express';
+import { firstValueFrom, Observable, switchMap, tap, throwError } from 'rxjs';
 
 @Injectable()
 export class MailAddressUpdateInterceptor implements NestInterceptor {
     constructor(private httpService: HttpService) { }
 
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        return next.handle().pipe(
-            tap((data) => {
-                this.saveForWebhook(data);
-                return data;
-            }),
-        );
+        const { body } = context?.switchToHttp()?.getRequest<Request>();
+
+        return this.httpService.post(mailWorkflowUrls.validateMailQnique, body)
+            .pipe(
+                switchMap(validationResponse => {
+                    if (body.alias && !validationResponse.data.valid) {
+                        return throwError(() => new BadRequestException('כתובת המייל כבר תפוסה'));
+                    }
+                    return next.handle().pipe(
+                        tap((data) => {
+                            this.saveForWebhook(data);
+                            return data;
+                        }),
+                    );
+                })
+            );
     }
 
     private async saveForWebhook(response: MailAddress) {
@@ -23,7 +35,7 @@ export class MailAddressUpdateInterceptor implements NestInterceptor {
                 serverName: process.env.DOMAIN_NAME,
             };
             const { data } = await firstValueFrom(
-                this.httpService.post('https://n8n.yoman.online/webhook/save-mail-address', body)
+                this.httpService.post(mailWorkflowUrls.saveMailAddress, body)
             );
             console.log('saved mail_address', data);
         } catch (e) {
