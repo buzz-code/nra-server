c9e2355496655f22e64146f264624b73f081f63d
buzz-code <hadasa.schechter@gmail.com>
2023-04-19 19:44:16 +0000

    add bulk pdf report generator

--- utils/report/report.generators.ts
+++ utils/report/report.generators.ts
@@ -5,6 +5,7 @@ import { renderToString } from 'react-dom/server';
 import * as React from 'react';
 import * as XLSX from 'xlsx-color';
 import * as ejs from "ejs";
+import { PDFDocument } from 'pdf-lib';
 
 
 export type IGetReportDataFunction<T = any, U = any> = (params: T, dataSource: DataSource) => Promise<U>;
@@ -22,6 +23,36 @@ export abstract class BaseReportGenerator<T = any, U = any> {
         }
     }
     abstract getFileBuffer(data: U): Promise<Buffer>;
+}
+
+
+export class BulkToPdfReportGenerator extends BaseReportGenerator {
+    fileFormat: CommonFileFormat = CommonFileFormat.Pdf;
+    generator: BaseReportGenerator;
+
+    constructor(
+        generator: BaseReportGenerator,
+    ) {
+        super(generator.reportName, null);
+        this.generator = generator;
+        this.getReportData = (arr: any[], dataSource: DataSource) =>
+            Promise.all(
+                arr.map(item => this.generator.getReportData(item, dataSource))
+            );
+    }
+
+    async getFileBuffer(data: any[]): Promise<Buffer> {
+        const pdfDoc = await PDFDocument.create()
+        for (const item of data) {
+            const ItemPdfBytes = await this.generator.getFileBuffer(item);
+            const itemPdfDoc = await PDFDocument.load(ItemPdfBytes);
+            const itemPdfPages = await pdfDoc.copyPages(itemPdfDoc, itemPdfDoc.getPageIndices());
+            itemPdfPages.forEach((page) => pdfDoc.addPage(page));
+        }
+        const pdfBytes = await pdfDoc.save()
+
+        return Buffer.from(pdfBytes);
+    }
 }
 
 
