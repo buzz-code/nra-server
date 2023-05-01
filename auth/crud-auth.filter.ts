7d0bdbec3d930a0992a89da11a5de3d35b079e90
buzz-code <hadasa.schechter@gmail.com>
2023-05-01 19:13:17 +0000

    add CrudAuthReadOnlyWithPermissionFunc

--- auth/crud-auth.filter.ts
+++ auth/crud-auth.filter.ts
@@ -1,4 +1,8 @@
-export const CrudAuthFilter = {
+import { AuthOptions } from "@dataui/crud";
+
+type AuthWithFunc = (permissionsFunc: (permissions: any) => boolean) => AuthOptions;
+
+export const CrudAuthFilter: AuthOptions = {
     filter: (user) => user.permissions.admin
         ? ({})
         : ({
@@ -6,14 +10,21 @@ export const CrudAuthFilter = {
         })
 };
 
-export const CrudAuthAdminFilter = {
+export const CrudAuthAdminFilter: AuthOptions = {
     filter: (user) => user.permissions.admin
         ? ({})
         : ({ id: -1 })
 }
 
-export const CrudAuthWithPermissionsFilter = (permissionsFunc: (permissions: any) => boolean) => ({
+export const CrudAuthWithPermissionsFilter: AuthWithFunc = (permissionsFunc) => ({
     filter: (user) => user.permissions.admin || permissionsFunc(user.permissions)
         ? ({})
         : ({ id: -1 })
+})
+
+export const CrudAuthReadOnlyWithPermissionFunc: AuthWithFunc = (permissionsFunc) => ({
+    filter: CrudAuthFilter.filter,
+    persist: (user) => user.permissions.admin || permissionsFunc(user.permissions)
+        ? ({})
+        : ({ id: 'break' })
 })
