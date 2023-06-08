83de586912b21ffb1fac115017d079aef107c4f0
buzz-code <hadasa.schechter@gmail.com>
2023-06-08 12:57:00 +0000

    use exceljs for excel file generation

--- utils/report/report.generators.ts
+++ utils/report/report.generators.ts
@@ -3,11 +3,11 @@ import { DataSource } from "typeorm";
 import puppeteer from 'puppeteer';
 import { renderToString } from 'react-dom/server';
 import * as React from 'react';
-import * as XLSX from 'xlsx-color';
 import * as ejs from "ejs";
 import { PDFDocument } from 'pdf-lib';
 import { getFileExtension } from "./report.util";
 import * as JSZip from 'jszip';
+import * as ExcelJS from 'exceljs';
 
 
 export type IGetReportDataFunction<T = any, U = any> = (params: T, dataSource: DataSource) => Promise<U>;
@@ -165,32 +165,26 @@ export class DataToExcelReportGenerator extends BaseReportGenerator<IDataToExcel
     fileFormat: CommonFileFormat = CommonFileFormat.Excel;
 
     async getFileBuffer(data: IDataToExcelReportGenerator): Promise<Buffer> {
-        const ws = XLSX.utils.aoa_to_sheet([data.headerRow]);
-        XLSX.utils.sheet_add_aoa(ws, data.formattedData, { origin: -1 });
+        const workbook = new ExcelJS.Workbook();
+        const sheetName = data.sheetName || 'גליון1';
+        const worksheet = workbook.addWorksheet(sheetName);
+        worksheet.views = [
+            { state: 'frozen', xSplit: 1, rightToLeft: true }
+        ];
+        worksheet.addTable({
+            name: 'data',
+            ref: 'A1',
+            headerRow: true,
+            style: {
+                theme: 'TableStyleMedium2',
+                showRowStripes: true,
+            },
+            columns: data.headerRow.map(name => ({ name })),
+            rows: data.formattedData,
+        });
 
-        for (let index = 0; index < data.headerRow.length; index++) {
-            var cell_ref = XLSX.utils.encode_cell({ c: index, r: 0 });
-            ws[cell_ref].s = {
-                font: {
-                    bold: true
-                },
-                fill: {
-                    fgColor: { rgb: "f2f2f2" }
-                }
-            }
-        }
-
-        const wb = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(wb, ws, data.sheetName || 'גליון1');
-
-        wb.Workbook ??= {}
-        wb.Workbook.Views ??= [{}]
-        wb.Workbook.Views.forEach((view) => {
-            view.RTL = true;
-        })
-
-        const buffer = XLSX.write(wb, { type: "buffer", bookType: "xlsx" });
-        return buffer;
+        const buffer = await workbook.xlsx.writeBuffer();
+        return Buffer.from(buffer);
     }
 }
 
