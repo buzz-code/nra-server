d181805dcb0c6a07598a127fea44682090dc9368
buzz-code <hadasa.schechter@gmail.com>
2023-09-10 07:11:57 +0000

    add MaxCountByUserLimit validator

--- utils/validation/class-validator-he.ts
+++ utils/validation/class-validator-he.ts
@@ -4,6 +4,7 @@ import {
     MaxLength as _MaxLength,
 } from "class-validator";
 import { IsUniqueCombination as _IsUniqueCombination } from "./is-unique-combination";
+import { GetMaxLimitType, MaxCountByUserLimit as _MaxCountByUserLimit } from "./max-count-by-user-limit";
 
 export const IsNotEmpty = (validationOptions?: ValidationOptions): PropertyDecorator =>
     _IsNotEmpty({ ...validationOptions, message: 'הערך של $property לא יכול להיות ריק' });
@@ -11,3 +12,5 @@ export const MaxLength = (max: number, validationOptions?: ValidationOptions): P
     _MaxLength(max, { ...validationOptions, message: '$property לא יכול להיות ארוך יותר מ-$constraint1 תווים' });
 export const IsUniqueCombination = (otherProperties: string[] = [], entities: Function[] = [], validationOptions?: ValidationOptions) =>
     _IsUniqueCombination(otherProperties, entities, { ...validationOptions, message: 'קיימת כבר רשומה עם ערכים זהים למשתמש בשדות $constraint1' });
+export const MaxCountByUserLimit = (entity: Function, getMaxLimit: GetMaxLimitType, entities: Function[] = [], foreignKey = 'id', validationOptions?: ValidationOptions) =>
+    _MaxCountByUserLimit(entity, getMaxLimit, entities, foreignKey, { ...validationOptions, message: 'לא ניתן ליצור עוד רשומות - הגבלת כמות לטבלת $constraint1' });
---
+++ utils/validation/max-count-by-user-limit.ts
@@ -0,0 +1,43 @@
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+import { DataSource, Not } from "typeorm";
+import { getDataSource } from '../entity/foreignKey.util';
+import { getUserIdFromUser } from '@shared/auth/auth.util';
+import { getCurrentUser } from './util';
+
+export type GetMaxLimitType = (userId: any, dataSource: DataSource) => Promise<Number>;
+export function MaxCountByUserLimit(entity: Function, getMaxLimit: GetMaxLimitType, entities: Function[] = [], foreignKey = 'id', validationOptions?: ValidationOptions) {
+    return function (object: Object, propertyName: string) {
+        registerDecorator({
+            name: MaxCountByUserLimit.name,
+            target: object.constructor,
+            propertyName: propertyName,
+            constraints: [entity.name],
+            options: {
+                message: 'max limit reached for $constraint1',
+                ...validationOptions
+            },
+            validator: {
+                async validate(value: any, args: ValidationArguments) {
+                    const fullObject = args.object as any;
+                    const user = getCurrentUser();
+                    const entityFilter: any = {
+                        userId: fullObject.userId ?? getUserIdFromUser(user),
+                    };
+                    if (fullObject[foreignKey]) {
+                        entityFilter.id = Not(fullObject[foreignKey]);
+                    }
+                    let dataSource: DataSource;
+                    try {
+                        dataSource = await getDataSource([entity, ...entities]);
+                        const maxLimit = await getMaxLimit(entityFilter.userId, dataSource);
+                        const count = await dataSource.getRepository(entity)
+                            .countBy(entityFilter);
+                        return count < maxLimit;
+                    } finally {
+                        dataSource.destroy();
+                    }
+                },
+            },
+        });
+    };
+}
