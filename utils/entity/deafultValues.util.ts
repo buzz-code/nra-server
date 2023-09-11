c017957529fc5f84fded584cc12af4f50766d3d5
buzz-code <hadasa.schechter@gmail.com>
2023-09-11 19:54:56 +0000

    add cleanDateFields function

--- utils/entity/deafultValues.util.ts
+++ utils/entity/deafultValues.util.ts
@@ -3,3 +3,11 @@ export function fillDefaultReportDateValue(item: { id: number, reportDate: Date 
         item.reportDate = new Date();
     }
 }
+
+export function cleanDateFields(item: any, fields: string[]) {
+    for (const field of fields) {
+        if (item[field] && typeof item[field] === 'string' && item[field].length === 24) {
+            item[field] = item[field].substr(0, 10);
+        }
+    }
+}
