66a053c5ffd7b92caaca1d4d8cf0d0ccc3aa60b9
buzz-code <hadasa.schechter@gmail.com>
2023-09-26 17:14:15 +0000

    add cache to database config

--- config/database.config.ts
+++ config/database.config.ts
@@ -1,5 +1,6 @@
 import { join } from "path";
 import { DataSourceOptions } from "typeorm";
+import { KeyvCacheProvider } from "typeorm-cache";
 
 export const databaseConfig: DataSourceOptions = {
     type: 'mysql',
@@ -15,6 +16,14 @@ export const databaseConfig: DataSourceOptions = {
     // synchronize: true,
     charset: "utf8mb4_0900_ai_ci",
     logging: "all",
+    cache: {
+        duration: 300_000,
+        provider() {
+            return new KeyvCacheProvider({
+                ttl: 300_000,
+            });
+        }
+    },
     migrationsRun: process.env.NODE_ENV == 'production' ||
         // true ||
         false,
