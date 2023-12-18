f76a38c918b61c867fab4d615adc2a521f96030c
buzz-code <hadasa.schechter@gmail.com>
2023-12-18 19:34:22 +0000

    add log for long queries

--- config/database.config.ts
+++ config/database.config.ts
@@ -2,6 +2,8 @@ import { join } from "path";
 import { DataSourceOptions } from "typeorm";
 import { KeyvCacheProvider } from "typeorm-cache";
 
+const cacheTTL = 300_000;
+
 export const databaseConfig: DataSourceOptions = {
     type: 'mysql',
     extra: {
@@ -16,11 +18,12 @@ export const databaseConfig: DataSourceOptions = {
     // synchronize: true,
     charset: "utf8mb4_0900_ai_ci",
     logging: "all",
+    maxQueryExecutionTime: 200,
     cache: {
-        duration: 300_000,
+        duration: cacheTTL,
         provider() {
             return new KeyvCacheProvider({
-                ttl: 300_000,
+                ttl: cacheTTL,
             });
         }
     },
