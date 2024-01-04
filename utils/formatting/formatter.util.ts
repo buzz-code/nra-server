cbc1ab0c534ea0e17e0812e8ca93cbfac6a18c29
buzz-code <hadasa.schechter@gmail.com>
2024-01-04 09:48:16 +0000

    add hebrew date formatter

--- utils/formatting/formatter.util.ts
+++ utils/formatting/formatter.util.ts
@@ -1,5 +1,13 @@
+import { formatJewishDateInHebrew, toJewishDate } from 'jewish-date';
+
 export const getPercentsFormatter = (value: string) =>
     row => formatPercent(row[value]);
 
 export const formatPercent = (value: any) =>
     value && !isNaN(value) ? `${Math.round(Number(value) * 100)}%` : null;
+
+export const getHebrewDateFormatter = (value: string) =>
+    row => formatHebrewDate(row[value]);
+
+export const formatHebrewDate = (value: any) =>
+    value ? formatJewishDateInHebrew(toJewishDate(new Date(value))) : null;
