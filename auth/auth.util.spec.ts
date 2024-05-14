d8154de769d396f39acd63041bc2a64afa1903a3
buzz-code <hadasa.schechter@gmail.com>
2024-05-14 14:57:38 +0000

    add tests to auth.util

---
+++ auth/auth.util.spec.ts
@@ -0,0 +1,23 @@
+import { getUserIdFromUser } from "./auth.util";
+
+describe('getUserIdFromUser', () => {
+    it('get undefined if user is null', () => {
+        expect(getUserIdFromUser(null)).toBeUndefined();
+    })
+
+    it('get undefined if user is admin', () => {
+        expect(getUserIdFromUser({ permissions: { admin: true } })).toBeUndefined();
+    })
+
+    it('get effective_id if user is not admin', () => {
+        expect(getUserIdFromUser({ effective_id: 1 })).toBe(1);
+    })
+
+    it('get id if user is not admin and effective_id is not defined', () => {
+        expect(getUserIdFromUser({ id: 1 })).toBe(1);
+    })
+
+    it('get undefined if user is not admin and effective_id and id are not defined', () => {
+        expect(getUserIdFromUser({})).toBeUndefined();
+    })
+})
--- auth/auth.util.ts
+++ auth/auth.util.ts
@@ -1,5 +1,5 @@
 export function getUserIdFromUser(user: any) {
     if (!user) return undefined;
-    if (user.permissions.admin) return undefined;
-    return user.effective_id || user.id;
+    if (user.permissions?.admin) return undefined;
+    return user.effective_id || user.id || undefined;
 }
