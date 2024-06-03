73f37b3e97229d7fe2b89120d0d15c50055c4d92
buzz-code <hadasa.schechter@gmail.com>
2024-06-03 13:33:42 +0000

    increase test coverage

---
+++ auth/public.decorator.spec.ts
@@ -0,0 +1,21 @@
+import { IS_PUBLIC, Public } from './public.decorator';
+
+describe('Public Decorator', () => {
+    it('should set IS_PUBLIC metadata to true on a method', () => {
+        class TestClass {
+            @Public()
+            testMethod() { }
+        }
+
+        const isPublicMetadata = Reflect.getMetadata(IS_PUBLIC, TestClass.prototype.testMethod);
+        expect(isPublicMetadata).toBe(true);
+    });
+
+    it('should set IS_PUBLIC metadata to true on a class', () => {
+        @Public()
+        class TestClass { }
+
+        const isPublicMetadata = Reflect.getMetadata(IS_PUBLIC, TestClass);
+        expect(isPublicMetadata).toBe(true);
+    });
+});
---
+++ config/database.config.spec.ts
@@ -0,0 +1,15 @@
+import { KeyvCacheProvider } from "typeorm-cache";
+import { databaseConfig } from "./database.config";
+
+describe("databaseConfig", () => {
+    it("should be defined", () => {
+        expect(databaseConfig).toBeDefined();
+    });
+
+    it("should have cache provider", () => {
+        const cacheProvider = (databaseConfig.cache as any)?.provider;
+        expect(cacheProvider).toBeDefined();
+        expect(typeof cacheProvider).toBe("function");
+        expect(cacheProvider()).toBeInstanceOf(KeyvCacheProvider);
+    });
+});
