98426e6f9e963374adb16c7a7a1c661171b18be6
buzz-code <hadasa.schechter@gmail.com>
2024-11-14 08:31:31 +0000

    enhance NumberType transformation to handle NaN values

--- utils/entity/class-transformer.ts
+++ utils/entity/class-transformer.ts
@@ -1,7 +1,9 @@
 import { Transform, TransformFnParams, Type } from "class-transformer";
 
 export const StringType = Type(() => String);
-export const NumberType = Type(() => Number);
+export const NumberType = Transform((params: TransformFnParams) => {
+    return isNaN(Number(params.value)) ? params.value : Number(params.value);
+});
 export const DateType = Transform((params: TransformFnParams) => {
     return isNaN(Date.parse(params.value)) ? params.value : new Date(params.value);
 });
