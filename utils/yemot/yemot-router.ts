7d46c2cc1fbfd4739b5fd9921c0342e28efd8d99
buzz-code <hadasa.schechter@gmail.com>
2025-05-02 08:29:18 +0000

    Enhance YemotRouter configuration with improved error handling and default settings

--- utils/yemot/yemot-router.ts
+++ utils/yemot/yemot-router.ts
@@ -1,4 +1,4 @@
-import { CallHandler, YemotRouter } from 'yemot-router2';
+import { Call, CallHandler, YemotRouter } from 'yemot-router2';
 import { Logger } from '@nestjs/common';
 import * as express from 'express';
 
@@ -12,10 +12,14 @@ export const setupYemotRouter = (callHandler: YemotCallHandler) => {
 
   const yemotRouter = YemotRouter({
     printLog: true,
+    timeout: 5 * 60 * 1000,
     uncaughtErrorHandler: (error, call) => {
       logger.error(`Uncaught error from ${call.phone}. Error: ${error.stack}`);
-      return call.id_list_message([{ type: 'text', data: 'אירעה שגיאה' }]);
-    }
+      return id_list_message_with_hangup(call, 'אירעה שגיאה, אנא נסה שוב מאוחר יותר');
+    },
+    defaults: {
+      removeInvalidChars: true,
+    },
   });
 
   router.use('/', yemotRouter.asExpressRouter);
@@ -33,4 +37,12 @@ export const setupYemotRouter = (callHandler: YemotCallHandler) => {
   });
 
   return router;
+}
+
+export const id_list_message = (call: Call, message: string) => {
+  call.id_list_message([{ type: 'text', data: message }], { prependToNextAction: true });
+}
+export const id_list_message_with_hangup = (call: Call, message: string) => {
+  call.id_list_message([{ type: 'text', data: message }], { prependToNextAction: true });
+  call.hangup();
 }
