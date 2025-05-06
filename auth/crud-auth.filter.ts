ec75d2c076cc485e6f827700472728f01ed258f9
buzz-code <hadasa.schechter@gmail.com>
2025-05-06 15:35:09 +0000

    Refactor auth filters to use constants for filter responses

--- auth/crud-auth.filter.ts
+++ auth/crud-auth.filter.ts
@@ -3,28 +3,34 @@ import { getUserIdFromUser } from "./auth.util";
 
 type AuthWithFunc = (permissionsFunc: (permissions: any) => boolean) => AuthOptions;
 
+const ADMIN_FILTER = {};
+const NO_DATA_FILTER = { id: -1 };
+const READ_ONLY_FILTER = { id: 'break' };
+const getUserIdFilter = (user) => ({
+    userId: getUserIdFromUser(user),
+});
+
 export const CrudAuthFilter: AuthOptions = {
     filter: (user) => user.permissions.admin
-        ? ({})
-        : ({
-            userId: getUserIdFromUser(user),
-        })
+        ? ADMIN_FILTER
+        : getUserIdFilter(user)
 };
 
 export const CrudAuthAdminFilter: AuthOptions = {
     filter: (user) => user.permissions.admin
-        ? ({})
-        : ({ id: -1 })
+        ? ADMIN_FILTER
+        : NO_DATA_FILTER
 }
 
 export const CrudAuthWithPermissionsFilter: AuthWithFunc = (permissionsFunc) => ({
     filter: (user) => user.permissions.admin || permissionsFunc(user.permissions)
-        ? ({})
-        : ({ id: -1 })
+        ? ADMIN_FILTER
+        : NO_DATA_FILTER
 })
 
 export const CrudAuthReadOnlyWithPermissionFunc: AuthWithFunc = (permissionsFunc) => ({
     persist: (user) => user.permissions.admin || permissionsFunc(user.permissions)
-        ? ({})
-        : ({ id: 'break' })
+        ? ADMIN_FILTER
+        : READ_ONLY_FILTER
 })
+
