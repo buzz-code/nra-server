78d55e0d37c4c1407772a8a94d3f978af0916e35
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2026-02-23 08:55:40 +0000

    feat: add getDateRange function to generate an array of dates between two dates

--- utils/entity/filters.util.ts
+++ utils/entity/filters.util.ts
@@ -19,3 +19,16 @@ export function dateFromString(dateStr: string): Date {
     }
     return dateObj;
 }
+
+export function getDateRange(startDate: Date, endDate: Date): Date[] {
+    const current = new Date(startDate);
+    current.setHours(0, 0, 0, 0);
+    const end = new Date(endDate);
+    end.setHours(23, 59, 59, 999);
+    const dates: Date[] = [];
+    while (current <= end) {
+        dates.push(new Date(current));
+        current.setDate(current.getDate() + 1);
+    }
+    return dates;
+}
